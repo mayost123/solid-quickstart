e1d0d1e7b37c110fe7f0c3ab59d4b66ef61c4a29
mayost123 <144786451+mayost123@users.noreply.github.com>
2023-09-12 14:01:27 +0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { Title } from "solid-start";
 import Counter from "~/components/Counter";
-<script src="https://app.wotnot.io/chat-widget/7QupRYZZrbxe082124717201ULDQJMgN.js" defer></script>
+
 export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
       <h1>Hello world!</h1>
-  
+  <script src="https://app.wotnot.io/chat-widget/7QupRYZZrbxe082124717201ULDQJMgN.js" defer></script>
       <Counter />
       <p>
         Visit{" "}
